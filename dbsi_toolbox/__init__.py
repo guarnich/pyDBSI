3b6c2a658dadf30cb97cd788cc45735d4fa68b0d
Francesco Guarnaccia <francescoguarnaccia@MacBook-Air-di-Francesco-3.local>
2025-11-17 10:53:29 +0100

    feat: model.py update

--- dbsi_toolbox/__init__.py
+++ dbsi_toolbox/__init__.py
@@ -5,8 +5,6 @@ from .nonlinear import DBSI_NonLinear
 from .common import DBSIParams
 from .utils import load_dwi_data_dipy, save_parameter_maps
 
-# Default Model Alias
-DBSIModel = DBSI_Linear
 
 __version__ = "0.1.0"
 print("DBSI Toolbox v0.1.0 loaded.")
